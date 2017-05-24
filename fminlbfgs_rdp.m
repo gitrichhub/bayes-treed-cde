e9b445383ce57919834ec90449381e493e53d64e
Richard Payne <paynedrichard@gmail.com>
2017-05-24 09:01:50 -0500

    Add license and copyright

--- fminlbfgs_rdp.m
+++ fminlbfgs_rdp.m
@@ -116,7 +116,7 @@ function [x,fval,exitflag,output,grad]=fminlbfgs_rdp(funfcn,x_init,optim)
 %  See also OPTIMSET, FMINSEARCH, FMINBND, FMINCON, FMINUNC, @, INLINE.
 %
 %  Function is written by D.Kroon University of Twente (Updated Nov. 2010)
-
+%
 % 2010-10-29 Aki Vehtari : GradConstr is 'off' by default.
 % 2011-9-28 Ville Tolvanen : Reduce step size until function returns finite
 %                            value
@@ -147,6 +147,24 @@ function [x,fval,exitflag,output,grad]=fminlbfgs_rdp(funfcn,x_init,optim)
 %CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 %ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 %POSSIBILITY OF SUCH DAMAGE.
+% 
+%     This file is part of bayes-treed-cde.
+% 
+%     bayes-treed-cde is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     bayes-treed-cde is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with bayes-treed-cde.  If not, see <http://www.gnu.org/licenses/>.
+%
+%     Copyright 2016-2017, Richard Payne
+
 
 % Read Optimization Parameters
 defaultopt = struct('Display','final',...
--- mfunc.m
+++ mfunc.m
@@ -1,3 +1,21 @@
+%     This file is part of bayes-treed-cde.
+% 
+%     bayes-treed-cde is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     bayes-treed-cde is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with bayes-treed-cde.  If not, see <http://www.gnu.org/licenses/>.
+%
+%     Copyright 2016-2017, Richard Payne
+
+
 function m = mfunc(Tind,Tmultiset)
     m = 1;
     % If the likelihood matches, they may be the same tree.
